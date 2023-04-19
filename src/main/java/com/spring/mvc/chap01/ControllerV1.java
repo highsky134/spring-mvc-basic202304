3bc2c072248906576561e8c13124f85bd2a1c42b
highsky134 <sangchun134@gmail.com>
2023-04-19 11:43:55 +0900

    # 1. 요청데이터 읽기(쿼리스트링, 커맨드객체, 경로문자열

--- src/main/java/com/spring/mvc/chap01/ControllerV1.java
+++ src/main/java/com/spring/mvc/chap01/ControllerV1.java
@@ -2,6 +2,7 @@ package com.spring.mvc.chap01;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -56,4 +57,27 @@ public class ControllerV1 {
         return "";
     }
 
+    // == 3. 커맨드 객체 이용하기
+    // ==> 쿼리스트링의 양이 너무 많을 경우 또는 연관성이 있을경우
+    // ex)  /spring/order?oNum=20230419007-P&goods=구두&amount=3&price=5000....
+    // DTO 객체(클래스)를 만들어 처리
+
+    @RequestMapping("/order")
+    public String order(OrderRequestDTO dto) {
+        System.out.println("dto = " + dto);
+        return "";
+    }
+
+    // == 4. URL에 경로로 붙어있는 데이터 읽기
+    // ==> /spring/member/hong/107
+    //      hong이라는 유저의 107번 게시글을 읽고 싶음
+    @RequestMapping("/member/{userName}/{bNo}")
+    public String member(
+            @PathVariable String userName,
+            @PathVariable long bNo
+    ) {
+        System.out.println("userName = " + userName);
+        System.out.println("bNo = " + bNo);
+        return "";
+    }
 }
